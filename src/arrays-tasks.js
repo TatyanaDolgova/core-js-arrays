085f473e5b3ecfdfc6028964942dee5d5f42e823
Tatyana Dolgova <kylikovatatyana1993@mail.ru>
2024-01-05 18:14:31 +0500

    complete some tasks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -129,8 +129,12 @@ function getStringsLength(arr1) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+  const sum = arr.reduce((curr, acc) => curr + acc, 0);
+  return +(sum / arr.length).toFixed(2);
 }
 
 /**
@@ -143,8 +147,8 @@ function getAverage(/* arr */) {
  *    isSameLength(['orange', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  return arr.every((item) => item.length === arr[0].length);
 }
 
 /**
@@ -158,8 +162,8 @@ function isSameLength(/* arr */) {
  *    isValueEqualsIndex([2, 1, 0, 4, 5]) => true
  *    isValueEqualsIndex([10, 20, 30, 40, 50]) => false
  */
-function isValueEqualsIndex(/* arr */) {
-  throw new Error('Not implemented');
+function isValueEqualsIndex(arr) {
+  return arr.some((item, i) => item === i);
 }
 
 /**
